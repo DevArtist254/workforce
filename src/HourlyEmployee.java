8136810f24bf10e81d8b3c4ded9ffca87025daff
DevArtist <kevinwasonga415@outlook.com>
2024-01-25 04:57:39 +0300

    int

---
+++ src/HourlyEmployee.java
@@ -0,0 +1,13 @@
+public class HourlyEmployee extends Employee{
+
+    private double hourlyPayRate;
+
+    public HourlyEmployee(String name, String birthDate, String endDate, long employeeId, String hireDate, double hourlyPayRate) {
+        super(name, birthDate, endDate, employeeId, hireDate);
+        this.hourlyPayRate = hourlyPayRate;
+    }
+
+    public void retire(){
+
+    }
+}
